ecf1965fef3de555e9167897d6d76ecbfb9b13f3
shubham080901 <168578669+shubham080901@users.noreply.github.com>
2024-07-27 18:16:46 +0530

    Delete SocialMediaPlatform directory

--- SocialMediaPlatform/src/main/java/com/twitter/SocialMediaPlatform/repository/ICommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.twitter.SocialMediaPlatform.repository;
-
-import com.twitter.SocialMediaPlatform.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICommentRepository extends JpaRepository<Comment, Long> {
-}
--- SocialMediaPlatform/src/main/java/com/twitter/SocialMediaPlatform/repository/IPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.twitter.SocialMediaPlatform.repository;
-
-import com.twitter.SocialMediaPlatform.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPostRepository extends JpaRepository<Post, Long> {
-}
